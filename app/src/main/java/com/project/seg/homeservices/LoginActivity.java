d795195a7e842e80d352b5b0c6962748fc34f941
Jeremy Fang <jfang022@uottawa.ca>
2018-10-23 18:31:46 -0400

    Revert "Worked on the database handler class. Created functions to check for a valid email, and to see if an account exists or not"
    This reverts commit 9d995bfe2834ff7f9ac3508535d2f4f26ecb20a7.

--- app/src/main/java/com/project/seg/homeservices/LoginActivity.java
+++ app/src/main/java/com/project/seg/homeservices/LoginActivity.java
@@ -1,7 +1,6 @@
 package com.project.seg.homeservices;
 
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
@@ -10,15 +9,5 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    /**
-     * Attempts to login to the app. DBHandler is used to see
-     * If the input fields contain values corresponding to an entry
-     * * in the database
-     * @param view login activity
-     */
-    public void attemptLogin(View view) {
-        DBHandler dbHandler = new DBHandler(this);
     }
 }
