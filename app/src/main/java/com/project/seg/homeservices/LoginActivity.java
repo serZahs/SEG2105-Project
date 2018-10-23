9d995bfe2834ff7f9ac3508535d2f4f26ecb20a7
Jeremy Fang <jfang022@uottawa.ca>
2018-10-23 18:29:59 -0400

    Worked on the database handler class. Created functions to check for a valid email, and to see if an account exists or not

--- app/src/main/java/com/project/seg/homeservices/LoginActivity.java
+++ app/src/main/java/com/project/seg/homeservices/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.project.seg.homeservices;
 
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
@@ -9,5 +10,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Attempts to login to the app. DBHandler is used to see
+     * If the input fields contain values corresponding to an entry
+     * * in the database
+     * @param view login activity
+     */
+    public void attemptLogin(View view) {
+        DBHandler dbHandler = new DBHandler(this);
     }
 }
