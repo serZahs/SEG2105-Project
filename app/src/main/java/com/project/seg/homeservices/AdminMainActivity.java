08e5e4daf15f8f3fbe7583740efadc5278f1a4bb
Shahzaib <codebasileus@protonmail.com>
2018-10-25 10:29:03 -0400

    Added welcome message implementation for Admin

--- app/src/main/java/com/project/seg/homeservices/AdminMainActivity.java
+++ app/src/main/java/com/project/seg/homeservices/AdminMainActivity.java
@@ -1,7 +1,10 @@
 package com.project.seg.homeservices;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class AdminMainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class AdminMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_main);
+        setWelcomeMsg();
+    }
+
+    private void setWelcomeMsg() {
+        TextView wMsg = findViewById(R.id.welcomeMsg);
+        Intent intent = getIntent();
+        String username = intent.getExtras().getString("username");
+        wMsg.setText("Welcome " + username);
     }
 }
